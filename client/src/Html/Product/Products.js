8913b78b7bc778233a73b9f8266756a1e66b53b3
ahsan.btph123@gmail.com <ahsan.btph123@gmail.com>
2022-05-23 00:20:49 +0500

    Products fixed

--- client/src/Html/Product/Products.js
+++ client/src/Html/Product/Products.js
@@ -46,36 +46,6 @@ export default function Products() {
                     <span className="cart-item-count">1</span>
                 </div>
             </div>
-            {/* sidebar */}
-            <div className="sidebar-overlay">
-                <aside className="sidebar">
-                    {/* close */}
-                    <button className="sidebar-close">
-                        <i className="fas fa-times" />
-                    </button>
-                    {/* links */}
-                    <ul className="sidebar-links">
-                        <li>
-                            <a href="index.html" className="sidebar-link">
-                                <i className="fas fa-home fa-fw" />
-                                home
-                            </a>
-                        </li>
-                        <li>
-                            <a href="products.html" className="sidebar-link">
-                                <i className="fas fa-couch fa-fw" />
-                                products
-                            </a>
-                        </li>
-                        <li>
-                            <a href="about.html" className="sidebar-link">
-                                <i className="fas fa-book fa-fw" />
-                                about
-                            </a>
-                        </li>
-                    </ul>
-                </aside>
-            </div>
             {/* cart */}
             <div className="cart-overlay">
                 <aside className="cart">
@@ -97,7 +67,7 @@ export default function Products() {
                 </aside>
             </div>
             {/* products */}
-            <section className="products">
+            <section className="productss">
                 {/* filters */}
                 <div className="filters">
                     <div className="filters-container">
