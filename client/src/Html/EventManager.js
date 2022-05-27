738e8c85ff7eb9f023ef0b60d76de94e2cd43ba6
ahsan.btph123@gmail.com <ahsan.btph123@gmail.com>
2022-05-27 11:51:01 +0500

    Event update fix

--- client/src/Html/EventManager.js
+++ client/src/Html/EventManager.js
@@ -37,7 +37,7 @@ export default function EventManager() {
         let userId = data2.id;
             let imagesUrl = JSON.parse(localStorage.getItem("imagesUrl"));
             let images = imagesUrl.urls;
-        let jsonObject = JSON.stringify({ "id": id, "userID": userId, "productType":"event", "fields": { "company": company, "colors": ["#f15025", "#222"], "price": document.getElementById("ProductPrice").value, "name": document.getElementById("ProductName").value, "imageUrl": imageUrl, "description": document.getElementById("description") }, "imagesUrl" : images });
+        let jsonObject = JSON.stringify({ "id": id, "userID": userId, "productType":"event", "fields": { "company": company, "colors": ["#f15025", "#222"], "price": document.getElementById("ProductPrice").value, "name": document.getElementById("ProductName").value, "imageUrl": imageUrl, "description": document.getElementById("description").value }, "imagesUrl" : images });
             const response = await fetch("http://localhost:3000/api/updateProduct", {
             method: "PUT",
             headers: {
